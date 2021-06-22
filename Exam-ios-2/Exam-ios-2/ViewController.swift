cf8ce0ad520a39070c720895a8a559a1df5e0f2d
loich91 <loichubermont@yahoo.fr>
2021-06-22 10:41:11 +0200

    ajout projet ios 2

---
+++ Exam-ios-2/Exam-ios-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exam-ios-2
+//
+//  Created by Student06 on 22/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
